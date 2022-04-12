4c6cf10495b6c2f2567caa117bd833bd27994b49
gh952505667 <15951835295@163.com>
2022-04-12 16:10:50 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,8 +7,12 @@ num=${#uidarr[@]}
 smbtn="进入健康状况上报平台"
 url1="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login"
 url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
+
 for((i=0;i<num;i++))
 do
+
+case $i in
+0)
 curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
 udata=$(sed -n '11p' temp.txt)
 udata=${udata#*ptopid=}
@@ -18,7 +22,23 @@ sid="${udata#*&sid=}" #登录获取ptopid和sid
 sleep 2
 curl -d "did=1&men6=a&fun18=637" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
 sleep 2
-curl -d "@myvs.txt" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+curl -d "@myvs0.txt" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
 udata=$(sed -n '24,26p' temp.txt)
-echo "$udata" > result.html
+echo "$udata" >> result.html
+;;
+
+1)
+curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=907" -s $url1 -o temp.txt
+udata=$(sed -n '11p' temp.txt)
+udata=${udata#*ptopid=}
+udata=${udata%\"\}\}*}
+ptopid="${udata%&*}"
+sid="${udata#*&sid=}" #登录获取ptopid和sid
+sleep 2
+curl -d "did=1&men6=a&fun18=637" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+sleep 2
+curl -d "@myvs1.txt" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
+udata=$(sed -n '24,26p' temp.txt)
+echo "$udata" >> result.html
+;;
 done
